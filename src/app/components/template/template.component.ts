bb02bb29e013b6577628dbec7cd02ad851c35f84
valeriadelrio <valeriadlrio@gmail.com>
2017-08-25 15:09:54 -0300

    Uso de ngModel, ngSubmit. Referencias locales a los elementos HTML

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent{
 
   constructor() { }
 
-  ngOnInit() {
+
+  guardar(){
+    console.log("Formulario posteado")
   }
 
 }
