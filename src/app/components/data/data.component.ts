96f87fef9db5dd6a93df2100c4f1ab2f8a652025
valeriadelrio <valeriadlrio@gmail.com>
2017-08-29 18:02:34 -0300

    Validaciones personalizadas, validaciones de password

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -29,7 +29,10 @@ export class DataComponent {
                                         Validators.required,
                                         Validators.minLength(3)
                                       ]),
-        'apellido': new FormControl('', Validators.required ),
+        'apellido': new FormControl('', [
+                                          Validators.required,
+                                          this.noDelRio
+                                          ] ),
       }),
       'correo': new FormControl('', [
                                       Validators.required,
@@ -37,30 +40,57 @@ export class DataComponent {
                                     ]),
       'pasatiempos': new FormArray([
         new FormControl('Correr', Validators.required)
-      ])
+      ]),
+      'password1': new FormControl('',Validators.required),
+      'password2': new FormControl()
     })
 
     // this.forma.setValue(this.usuario);
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ])
 
   }
 
-  guardarCambios(){
-    console.log(this.forma.value);
-    console.log(this.forma);
-    this.forma.reset({
-      nombrecompleto: {
-        nombre:"",
-        apellido:"",
-      },
-      correo:""
-    });
-    // this.forma.controls['correo'].setValue()
-  }
 
   agregarPasatiempo(){
     (<FormArray>this.forma.controls['pasatiempos']).push(
     new FormControl('', Validators.required)
     )
   }
+
+  noDelRio(control: FormControl): {[s:string]:boolean}{
+    if(control.value==="del Rio"){
+      return{
+        nodelrio:true
+      }
+    }
+    return null;
+  }
+
+  noIgual(control: FormControl): {[s:string]:boolean}{
+    let forma:any=this;
+    if(control.value !== forma.controls['password1'].value){
+      return{
+        noiguales:true
+      }
+    }
+    return null;
+  }
+
+  guardarCambios(){
+    console.log(this.forma.value);
+    console.log(this.forma);
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre:"",
+    //     apellido:"",
+    //   },
+    //   correo:""
+    // });
+
+  }
+
 
 }
