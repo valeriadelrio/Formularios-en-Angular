85df8ecb98c0711e3de4202b971a55d2b77177db
valeriadelrio <valeriadlrio@gmail.com>
2017-08-29 16:01:19 -0300

    Agrupacion de los objetos - formGroupName

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -9,16 +9,31 @@ export class DataComponent {
 
   forma: FormGroup;
 
+  usuario:Object ={
+    nombrecompleto: {
+      nombre: "Valeria",
+      apellido: "del Rio"
+    },
+    correo: "valeriadelrio@gmail.com"
+  }
+
   constructor() {
+
+    console.log(this.usuario);
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('', Validators.required ),
-      'apellido': new FormControl('', Validators.required ),
+      'nombrecompleto':new FormGroup({
+        'nombre': new FormControl('', [
+                                        Validators.required,
+                                        Validators.minLength(3)
+                                      ]),
+        'apellido': new FormControl('', Validators.required ),
+      }),
       'correo': new FormControl('', [
                                       Validators.required,
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
                                     ])
     })
-
 
   }
 
