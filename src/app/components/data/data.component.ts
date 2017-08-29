4b4a07731f0c403aae722cadcba3839a87061373
valeriadelrio <valeriadlrio@gmail.com>
2017-08-29 12:00:59 -0300

    Creacion del formulario por data usando FormGroup

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('Valeria'),
+      'apellido': new FormControl(),
+      'correo': new FormControl()
+    })
+
+
+  }
+
+  guardarCambios(){
+    
   }
 
 }
