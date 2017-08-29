79b6a54b12f19e258c645c5409b9ae9c9f104a6a
valeriadelrio <valeriadlrio@gmail.com>
2017-08-29 12:11:03 -0300

    Enlace de inputs a propiedades del FormGroup y validaciones

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,16 +11,19 @@ export class DataComponent {
 
   constructor() {
     this.forma = new FormGroup({
-      'nombre': new FormControl('Valeria'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombre': new FormControl('', Validators.required ),
+      'apellido': new FormControl('', Validators.required ),
+      'correo': new FormControl('', [
+                                      Validators.required,
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                    ])
     })
 
 
   }
 
   guardarCambios(){
-    
+
   }
 
 }
